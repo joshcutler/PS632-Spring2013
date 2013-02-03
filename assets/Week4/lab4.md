0cb4a252a883a340b4e3f668f4be000b06eb0969
Matt Dickenson <mcdickenson@gmail.com>
2013-02-03 10:17:34 -0500

    began writing network code for lab4

--- assets/Week4/lab4.md
+++ assets/Week4/lab4.md
@@ -1,8 +1,11 @@
-First, play with dictionaries. (Look back at `roman_numify` code in `lab1.py`.)
+First, play with dictionaries using `roman_numify` code.
 
-Now, make a function that inverts Roman numify. You can do this the slow way (iterating through all keys and checking values) or the fast way (reverse lookup).
+Introduce the concept of an [Eulerian path](http://en.wikipedia.org/wiki/Eulerian_path) and an Eulerian tour.
 
-1. Implement linear search (Zelle 2002: 226)
-2. Implement binary search (Zelle 2002: 227)
+Discuss chain networks and ring networks. Also describe how these would be represented in an adjacency matrix.
 
-Then, go through a recursive version of binary search. 
+Now, explain `make_link()` and the `ring` network. 
+
+Discuss a grid network. Use the code to count how many edges a 16x16 grid network would have. 
+
+Explain that all these networks are instances of planar graphs: they can be represented in two dimensional space. In general, *n-m+r=2* where *r* is the number of regions, m is the number of edges, and n is the number of nodes. 
--- assets/Week4/lab4.py
+++ assets/Week4/lab4.py
@@ -1,3 +1,5 @@
+import math 
+
 def Romanify(num):
   """turns a base-10 integer between 0 and 4000 into
   a Roman numeral string"""
@@ -16,31 +18,110 @@ def Romanify(num):
 # thisYear = Romanify(2013)
 # print thisYear 
 
-def DeRomanify(string):
-  return 0 
-  # TODO: make this work! 
 
-def linear_search(x, nums):
-  """Linear search from Zelle (2002) p. 226"""
-  for i in range(len(nums)):
-    if nums[i] == x:
-      return i 
-  return -1 
+# Code from Udacity CS 215, Unit 2 
+def make_link(G, node1, node2):
+  if node1 not in G:
+    G[node1] = {}
+  (G[node1])[node2] = 1
+  if node2 not in G:
+    G[node2] = {}
+  (G[node2])[node1] = 1
+  return G 
 
-def binary_search(x, nums):
-  """Zelle (2002) p. 227; expects sorted list"""
-  low = 0 
-  high = len(nums) -1
-  while low < high: 
-    mid = (low+high)/2
-    item = nums[mid]
-    if x == item:
-      return mid
-    elif x < item: 
-      high = mid -1 
-    else: 
-      low = mid + 1
-  return -1 
+# Ring Network
+# Make an empty graph (dict)
+ring = {}
 
-print linear_search(1, [0,1,2,3])
-print binary_search(1, [0,1,2,3])
+n = 5
+
+# Add in edges
+for i in range(n):
+  ring = make_link(ring, i, (i+1)%n)
+
+# How many nodes?
+print len(ring)
+
+# How many edges?
+print sum([len(ring[node]) for node in ring.keys()])/2 
+
+print ring 
+
+
+# Grid Network
+grid = {}
+
+n = 256 
+side = int(math.sqrt(n))
+
+# Add in edges
+for i in range(side):
+  for j in range(side):
+    if i < side-1: make_link(grid, (i,j), (i+1,j)) # link to node below
+    if j < side-1: make_link(grid, (i,j), (i,j+1)) # link to node at right
+
+# How many nodes?
+print len(grid)
+
+# How many edges?
+print sum([len(grid[node]) for node in grid.keys()])/2 # make this a function
+
+def count_edges(graph):
+  return sum([len(graph[node]) for node in graph.keys()])/2
+
+# Social network
+class Actor(object):
+  def __init__(self, name):
+    self.name = name 
+
+  def __repr__(self):
+    return "My name is " + self.name 
+
+ss = Actor("Susan Sarandon")
+jr = Actor("Julia Roberts")
+kb = Actor("Kevin Bacon")
+ah = Actor("Anne Hathaway")
+rd = Actor("Robert DiNero")
+ms = Actor("Meryl Streep")
+dh = Actor("Dustin Hoffman")
+
+movies = {}
+
+make_link(movies, dh, rd) # Wag the Dog
+make_link(movies, rd, ms) # Marvin's Room
+make_link(movies, dh, ss) # Midnight Mile
+make_link(movies, dh, jr) # Hook
+make_link(movies, dh, kb) # Sleepers
+make_link(movies, ss, jr) # Stepmom
+make_link(movies, kb, jr) # Flatliners
+make_link(movies, kb, ms) # The River Wild
+make_link(movies, ah, ms) # Devil Wears Prada
+make_link(movies, ah, jr) # Valentine's Day
+
+
+print len(movies) # How many nodes?
+print count_edges(movies) # How many edges?
+
+def tour(graph, nodes):
+  for i in range(len(nodes)):
+    node = nodes[i] 
+    if node in graph.keys():
+      print node 
+    else:
+      print "Node not found!"
+      break 
+    if i+1 < len(nodes):
+      next_node = nodes[i+1]
+      if next_node in graph.keys():
+        if next_node in graph[node].keys():
+          pass 
+        else:
+          print "Can't get there from here!"
+          break 
+
+movie_tour = [ms, rd, dh, ss, jr, ah, ms, kb, dh, jr, kb] 
+# print rd in movies[ms]
+tour(movies, movie_tour)
+
+# TODO: write a function to find an Eulerian tour 
+
