6624f332cae20a8bf9b9ec91a84acd15952bb027
Matt Dickenson <mcdickenson@gmail.com>
2013-02-27 10:18:57 -0500

    added answers to first 2 questions in lab 7

---
+++ assets/Week7/lab7.md
@@ -0,0 +1,17 @@
+### Intro
+Make sure everyone understands:
+
+- `with`
+- `.query`
+- `.filter`
+- `.group_by`
+- `.order_by`
+- `.distinct()`
+
+### Geography Database
+
+Show the CSV version because that's what people are used to.
+
+1. Create the database: `sqlite3 geog.db`
+2. Create the tables: `python makeGeogTables.py`
+
--- assets/Week7/lab7answers.py
+++ assets/Week7/lab7answers.py
@@ -75,53 +75,59 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-session.add_all([
-	Town('A', 110000, 1),
-	Town('B', 80000, 3),
-	Town('C', 300000, 3),
-	Town('D', 50000, 2),
-	Town('E', 113000, 2),
-	Town('F', 70000, 1)
-])
+# session.add_all([
+# 	Town('A', 110000, 1),
+# 	Town('B', 80000, 3),
+# 	Town('C', 300000, 3),
+# 	Town('D', 50000, 2),
+# 	Town('E', 113000, 2),
+# 	Town('F', 70000, 1)
+# ])
 
-session.add_all([
-	Distance('A', 'E', 50),
-	Distance('A', 'F', 60),
-	Distance('B', 'C', 50),
-	Distance('B', 'D', 60),
-	Distance('C', 'B', 50),
-	Distance('D', 'B', 60),
-	Distance('D', 'E', 30),
-	Distance('E', 'A', 50),
-	Distance('E', 'B', 60),
-	Distance('E', 'D', 30),
-	Distance('E', 'F', 100),
-	Distance('F', 'A', 60)
-])
+# session.add_all([
+# 	Distance('A', 'E', 50),
+# 	Distance('A', 'F', 60),
+# 	Distance('B', 'C', 50),
+# 	Distance('B', 'D', 60),
+# 	Distance('C', 'B', 50),
+# 	Distance('D', 'B', 60),
+# 	Distance('D', 'E', 30),
+# 	Distance('E', 'A', 50),
+# 	Distance('E', 'B', 60),
+# 	Distance('E', 'D', 30),
+# 	Distance('E', 'F', 100),
+# 	Distance('F', 'A', 60)
+# ])
 
-session.add_all([
-	Department('Department 1', 1),
-	Department('Department 2', 1),
-	Department('Department 3', 3),
-	Department('Department 4', 2)
-])
+# session.add_all([
+# 	Department('Department 1', 1),
+# 	Department('Department 2', 1),
+# 	Department('Department 3', 3),
+# 	Department('Department 4', 2)
+# ])
 
-session.add_all([
-	Region('Region 1'),
-	Region('Region 2'),
-	Region('Region 3')
-])
+# session.add_all([
+# 	Region('Region 1'),
+# 	Region('Region 2'),
+# 	Region('Region 3')
+# ])
 
-session.commit()
+# session.commit()
 
 # Some querying 
 for town in session.query(Town).order_by(Town.id):
 	print town.name, town.population
-
-# TODO: 
+ 
 # 1. Display, by department, the cities having more than 100000 inhabitants.
+for t in session.query(Town).filter(Town.population>100000).group_by(Town.numdept).order_by(Town.numdept):
+	print "Department", t.numdept, "Town", t.name
+
 # 2. Display the list of all the one-way connections between two cities for which the population of one of the 2 cities is lower than 80000 inhabitants. 
+for d in session.query(Town, Distance).filter(Town.population<80000).filter(or_(Distance.townarrive==Town.name, Distance.towndepart==Town.name)).order_by(Town.name).distinct():
+	print d[1]
+
 # 3. Display the list of cities 2 road sections apart, and the distance which separates them.
+
 # 4. Display the list of cities 4 road sections apart (there can be shorter connections of 2 or 3 sections), and the distance which separates them.
 # 5. Display the number of inhabitants per region and department. Suppose that the population of a region is that of the cities which are part of the road network.
 # 6. Give the name of the region which has the longest road network and the number of kilometers of this network. 
