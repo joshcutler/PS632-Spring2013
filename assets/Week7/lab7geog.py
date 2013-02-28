5c575f87ec9ede03d002ace8bff2b8d442a4e5e3
Matt Dickenson <mcdickenson@gmail.com>
2013-02-28 13:57:10 -0500

    updated lab 7 geography file with distance-related task

--- assets/Week7/lab7geog.py
+++ assets/Week7/lab7geog.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, and_, or_, func
 from sqlalchemy.orm import relationship, backref, sessionmaker, aliased
 
 #Connect to the local database
-engine = sqlalchemy.create_engine('sqlite:///geog.db', echo=True)
+engine = sqlalchemy.create_engine('sqlite:///geog.db', echo=False)
 
 Base = declarative_base() 
 
@@ -52,6 +52,28 @@ class Town(Base):
 	def __repr__(self):
 		return "<Town('%s')>" % (self.name)
 
+class Distance(Base):
+	__tablename__ = 'distances'
+
+	id = Column(Integer, primary_key=True)
+	towndepart = Column(String, ForeignKey('towns.name'))
+	townarrive = Column(String, ForeignKey('towns.name'))
+	# could also use id's 
+	distance = Column(Integer)
+
+	td = relationship("Town", 
+		primaryjoin= towndepart == Town.name)
+	ta = relationship("Town", 
+		primaryjoin = townarrive == Town.name)
+	
+	def __init__(self, distance):
+		self.distance = distance 
+
+	def __repr__(self):
+		return "<Distance('%s', '%s', '%s')>" % (self.towndepart, self.townarrive, self.distance)
+
+
+
 #First time create tables
 Base.metadata.create_all(engine) 
 
@@ -82,6 +104,46 @@ session.add_all([dept1, dept2, dept3, dept4])
 
 # TODO: Create towns, nested in departments
 
+session.add_all([a,b,c,d,e,f])
+
+ae = Distance(50)
+ae.td, ae.ta = a, e 
+
+af = Distance(60)
+af.td, af.ta = a, f 
+
+bc = Distance(50)
+bc.td, bc.ta = b, c 
+
+bd = Distance(60)
+bd.td, bd.ta = b, d 
+
+cb = Distance(50)
+cb.td, cb.ta = c, b 
+
+db = Distance(60)
+db.td, db.ta = d, b 
+
+de = Distance(30)
+de.td, de.ta = d, e 
+
+ea = Distance(50)
+ea.td, ea.ta = e, a 
+
+eb = Distance(60)
+eb.td, eb.ta = e, b 
+
+ed = Distance(30)
+ed.td, ed.ta = e, d 
+
+ef = Distance(100)
+ef.td, ef.ta = e, f 
+
+fa = Distance(60)
+fa.td, fa.ta = f, a 
+
+session.add_all([ae, af, bc, bd, cb, db, de, ea, eb, ed, ef, fa])
+
 session.commit()
 
 # Some example querying 
@@ -92,3 +154,4 @@ for town in session.query(Town).order_by(Town.id):
 # 1. Display, by department, the cities having more than 100000 inhabitants.
 # 2. Display the list of all the one-way connections between two cities for which the population of one of the 2 cities is lower than 80000 inhabitants. 
 # 3. Display the number of inhabitants per department (bonus: do it per region as well). 
+# hint: use func.sum
